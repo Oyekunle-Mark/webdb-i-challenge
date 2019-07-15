290e21f0a7b4180a0cdee7000c58ce1ebff654d9
oyekunle-mark <oyekunlemac@gmail.com>
2019-07-15 16:22:39 +0100

    Used the cors compression logger and helmet middlewares

--- server.js
+++ server.js
@@ -1,10 +1,18 @@
 const express = require('express');
+const cors = require('cors');
+const compression = require('compression');
+const logger = require('morgan');
+const helmet = require('helmet');
 
 const server = express();
 const accountRouter = require('./router');
 const accountMiddleware = require('./middleware');
 
 server.use(express.json());
+server.use(cors());
+server.use(compression());
+server.use(logger('dev'));
+server.use(helmet());
 
 server.use('/api/accounts', accountRouter);
 server.use(accountMiddleware.badUrl);
